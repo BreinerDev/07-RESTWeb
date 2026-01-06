3fe59df8dc274320c5c0276138b08c40acbf722b
JuniorDev <barrazabreiner22@gmail.com>
2026-01-06 15:31:16 -0500

    primer commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,47 @@
+import express, { Router } from "express";
+import compression from "compression";
+import path from "path";
+
+interface Options {
+  port: number;
+  routes: Router;
+  public_path?: string;
+}
+
+export class Server {
+  private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
+  private readonly routes: Router;
+
+  constructor(options: Options) {
+    const { port, public_path = "public", routes } = options;
+    this.port = port;
+    this.routes = routes;
+    this.publicPath = public_path;
+  }
+
+  async start() {
+    //* Middlewares
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(compression());
+
+    //* Routes
+    this.app.use(this.routes);
+
+    //* Public Folder
+    this.app.use(express.static(this.publicPath));
+
+    this.app.get("/{*splat}", (req, res) => {
+      const indexPath = path.join(
+        __dirname + `../../../${this.publicPath}/index.html`
+      );
+      res.sendFile(indexPath);
+    });
+
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+}
---
+++ src/presentation/todos/controllers.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from "express";
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
+import {
+  CreateTodo,
+  CustomError,
+  DeleteTodo,
+  GetTodo,
+  GetTodos,
+  TodoEntity,
+  TodoRepository,
+  UpdateTodo,
+} from "../../domain";
+
+export class TodosController {
+  //* DI
+  constructor(private readonly todoRepository: TodoRepository) {}
+
+  private handleError = (res: Response, error: unknown) => {
+    if (error instanceof CustomError) {
+      res.status(error.statusCode).json({ error: error.message });
+      return;
+    }
+    res.status(500).json({ error: "Internal server error - check logs" });
+  };
+
+  public getTodos = (req: Request, res: Response) => {
+    new GetTodos(this.todoRepository)
+      .execute()
+      .then((todos) => res.json(todos))
+      .catch((error) => this.handleError(res, error));
+  };
+
+  public getByIdTodo = (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    new GetTodo(this.todoRepository)
+      .execute(+id)
+      .then((todo) => res.json(todo))
+      .catch((error: CustomError) => this.handleError(res, error));
+  };
+
+  public createTodo = (req: Request, res: Response) => {
+    const [error, createTodoDto] = CreateTodoDto.create(req.body);
+
+    if (error) return res.status(400).json({ error });
+
+    new CreateTodo(this.todoRepository)
+      .execute(createTodoDto!)
+      .then((todo) => res.json(todo))
+      .catch((error) => this.handleError(res, error));
+  };
+
+  public updateTodo = (req: Request, res: Response) => {
+    const id = +req.params.id;
+
+    const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id });
+
+    if (error) return res.status(400).json({ error });
+
+    new UpdateTodo(this.todoRepository)
+      .execute(updateTodoDto!)
+      .then((todo) => res.json(todo))
+      .catch((error) => this.handleError(res, error));
+  };
+
+  public deleteTodo = (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    new DeleteTodo(this.todoRepository)
+      .execute(+id)
+      .then((todo) => res.json(todo))
+      .catch((error) => this.handleError(res, error));
+  };
+}
